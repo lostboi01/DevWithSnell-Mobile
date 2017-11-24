d0d0bc7769656bd4de1025218b96a967e3fd5f05
Gerald Snell <snell.gerald@gmail.com>
2017-11-24 12:28:11 -0700

    Home Screen Update 2

--- Demo/src/main/cpp/native-lib.cpp
+++ Demo/src/main/cpp/native-lib.cpp
@@ -8,6 +8,6 @@ JNICALL
 Java_com_reactjs_devwithsnell_devwithsnell_Demo_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "DevWithSnell";
+    std::string hello = "DevWithSNELL";
     return env->NewStringUTF(hello.c_str());
 }
